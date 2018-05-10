e17e087a7985af8a62765588d09c882f9314d4a0
Swathi <swathinagaraja@gmail.com>
2018-05-10 22:04:41 +1000

    initial project structure

---
+++ src/test/java/parcelpointassignment/SampleTest.java
@@ -0,0 +1,15 @@
+package parcelpointassignment;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SampleTest
+{
+
+	@Test
+	public void testSample()
+	{
+		Assert.assertEquals("Sample Test".length(), 11);
+	}
+
+}
